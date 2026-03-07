58418dc14d530ff608525c22bed84f6c75a9fc25
shashi-singh-dev <shashisinghde@gmail.com>
2026-03-07 05:04:04 -0800

    Add Two Sum solution using two pointer approach

---
+++ TwoPointer/Two_Sum_sorted_array.py
@@ -0,0 +1,19 @@
+
+def two_sum(numbers,target,ans):
+    left=0
+    right=len(numbers) - 1
+    print(left)
+    print(right)
+    while left<right:
+        if numbers[left] + numbers[right] == target:
+            return left,right
+        elif numbers[left] + numbers[right] > target:
+            right = right - 1
+        else:
+            left = left + 1
+numbers = [2,7,11,15]
+target = 22
+ans=[]
+ans = two_sum(numbers,target,ans)
+print(f'The indexs are: {ans}')
+        
