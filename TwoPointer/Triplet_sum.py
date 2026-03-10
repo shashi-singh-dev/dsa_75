1a97fc7b3abeb967a7c0fca45af3195b3d1c0d94
shashi-singh-dev <shashisinghde@gmail.com>
2026-03-10 11:53:11 -0700

    Triplet_sum_two_pointer

---
+++ TwoPointer/Triplet_sum.py
@@ -0,0 +1,24 @@
+def triplet(num,target):
+    # ans=[]
+    for i in range(len(num)-1):
+        l=i+1
+        r=len(num)-1
+        while(l<r):
+            # print(i,l,r) this is to understand the loop flow
+            if num[i]+num[l]+num[r] == target:
+                print(num[i],num[l],num[r],num[i]+num[l]+num[r])
+                ans.append([num[i],num[l],num[r]])
+                l+=1
+                r-=1
+            elif num[i]+num[l]+num[r] < target:
+                l+=1
+            elif num[i]+num[l]+num[r] > target:
+                r-=1
+    return ans
+
+num = [-3,-2,-1,0,1,2,3]
+num.sort()
+target = 0
+ans=[]
+ans=triplet(num,target)
+print(ans)
